894759512d30572f8f23d19ffbfa16e981ca25cc
Nazar Karpa <Bonasane@gmail.com>
2025-09-27 12:22:38 +0300

    Base project template

---
+++ course_and_module_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CourseAndModuleAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'course_and_module_app'
---
+++ tasksmarks_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TasksmarksAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tasksmarks_app'
