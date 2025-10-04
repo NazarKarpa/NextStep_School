56f32400d2f578da62c891873799023ed4ad64f9
Nazar Karpa <Bonasane@gmail.com>
2025-10-04 10:29:22 +0300

    add 2 models in taskmarks_app

--- lessons_app/models.py
+++ lessons_app/models.py
@@ -1,12 +1,17 @@
-from tkinter.constants import CASCADE
-
 from django.db import models
+from django.db.models import CASCADE
 import course_and_module_app.models as module
+
+class Material(models.Model):
+    link = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
 
 class Lesson(models.Model):
     name = models.CharField(max_length=150)
-    material = models.TextField()
     modul = models.ForeignKey(module.Module, on_delete=CASCADE, related_name='Lessons')
+    material = models.ManyToManyField(Material, blank=True, related_name = 'materials')
 
     def __str__(self):
         return f'Lesson_name - {self.name}, modul - {self.modul}'
+
--- tasksmarks_app/models.py
+++ tasksmarks_app/models.py
@@ -1,4 +1,37 @@
 from django.db import models
+from django.db.models import CASCADE
 
-class Lesson(models.Model):
-    pass
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('un_check', 'Un check'),
+        ('well', 'Well Done!'),
+        ('wrong', 'Wrong, try again')
+    ]
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+
+    def __str__(self):
+        return f'Name - {self.name}, status - {self.status}'
+
+
+class TestTask(models.Model):
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('well', 'Well Done!'),
+        ('wrong', 'Wrong, try again')
+    ]
+    name = models.CharField(max_length=250)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+
+    def __str__(self):
+        return f'Name - {self.name}, status - {self.status}'
+
+class Option(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Варіант')
+    option = models.ForeignKey(TestTask, on_delete=CASCADE, related_name='answers', verbose_name='Вибір варіанту')
+
+    def __str__(self):
+        return f'Name" -{self.name},'
