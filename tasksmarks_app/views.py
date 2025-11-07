3a0545a2b2667c13411fec25e33ab7d9b5bf3f37
Nazar Karpa <Bonasane@gmail.com>
2025-11-07 17:47:50 +0200

    create task_form and add to html unworked with problems

--- tasksmarks_app/views.py
+++ tasksmarks_app/views.py
@@ -7,16 +7,23 @@ from django.http import HttpResponseRedirect
 from .mixins import *
 import tasksmarks_app.models
 from lessons_app.models import Lesson
+from tasksmarks_app.forms import *
 
 class LessonTaskView(TaskListMixin, TaskDetailMixins, LoginRequiredMixin, TemplateView):
     template_name = 'task/task_homework.html'
 
-    def get_context_data(self, **kwargs):
+    def get_lesson_context(self, ):
         lesson = Lesson.objects.all()
-        context = super().get_context_data(**kwargs)
+        context = {}
         context.update(self.get_task_list_context())
         for i in lesson:
             context.update(self.get_task_detail_context(task_id=i.id))
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task_form'] = TaskForm()
+        context.update(self.get_lesson_context())
         return context
 
 
