5d3e0527ad2443bf19a8d7fdfa7b85408eed0add
Nazar Karpa <Bonasane@gmail.com>
2025-11-08 12:18:11 +0200

    resolve bug in TaskListMixin, add base home html

--- home_app/views.py
+++ home_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-import mixins as mx
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
+from home_app.models import Announcement
 
 class AnnouncementListView(ListView):
-    pass
+    model = Announcement
+    context_object_name = 'announcements'
+    template_name = 'home/announcement_list.html'
--- tasksmarks_app/mixins.py
+++ tasksmarks_app/mixins.py
@@ -2,7 +2,7 @@ from .models import Task, TestTask
 
 class TaskListMixin:
     def get_task_list_context(self, lesson):
-        return {'tasks': Task.objects.filter(lesson=lesson)}
+        return {'tasks': Task.objects.filter(task_in_lesson=lesson)}
 
 class TaskDetailMixins:
     def get_task_detail_context(self, task_id):
