d07f178a2f40f231f342785dbf5dc50970c5568c
Nazar Karpa <Bonasane@gmail.com>
2025-11-14 08:10:23 +0200

    add LessonTaskListView

--- tasksmarks_app/forms.py
+++ tasksmarks_app/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from tasksmarks_app.models import *
 
-# class TaskForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = ['name', 'description', 'status']
-#
-#     def __init__(self, *args, **kwargs):
-#         super(TaskForm, self).__init__(*args, **kwargs)
-#         for field in self.fields:
-#             self.fields[field].widget.attrs.update({'class': 'form-control'})
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['user_answer']
+
+    user_answer = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 6,
+            'placeholder': 'Введіть вашу відповідь тут...',
+            'maxlength': '2000'
+        }),
+        label='Ваша відповідь',
+        required=True,
+        max_length=2000
+    )
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
--- tasksmarks_app/models.py
+++ tasksmarks_app/models.py
@@ -14,6 +14,7 @@ class Task(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    user_answer = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'Name - {self.name}, status - {self.status}'
